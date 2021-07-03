1c5b4c8002c1b5510350739d090da8b6b66ef041
chayezo <yejinchaa@naver.com>
2021-07-03 23:42:39 +0900

    Sat Jul 3 : Todo-list-react

--- todo-list/src/components/TodoListTemplate.js
+++ todo-list/src/components/TodoListTemplate.js
@@ -5,7 +5,7 @@ const TodoListTemplate = ({ children }) => {
     return (
         <div className='TodoListTemplate'>
             <div className='TodoHeadBlock'>
-                <div className='app-title'>일정 관리</div>
+                <div className='app-title'>WHAT TO DO</div>
                 <h1>2021년 07월 02일</h1>
             </div>
             <div className='content'>{children}</div>
